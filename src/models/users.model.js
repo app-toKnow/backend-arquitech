4c4ac3f4ce2899d6cc5c651a7410beea189ce5f9
disection <diseccionmexico@gmail.com>
2022-09-16 23:08:12 -0500

    ADD user model users

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,7 +1,80 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
 
+    name: {
+        type: String,
+        minlength: 3,
+        maxlength: 20,
+        required: true,
+        trim : true 
+    },
+    surname_p: {
+        type: String,
+        minlength: 3,
+        maxlength: 20,
+        required: true,
+        trim : true 
+    },
+    surname_m: {
+        type: String,
+        minlength: 3,
+        maxlength: 20,
+        required: true,
+        trim : true 
+    },
+    age: {
+        type: Number,
+        min: 5,
+        max: 13,
+        required: true
+    },
+    dob: {
+        type: Date, default: Date.now
+    },
+    pob: {
+        type: String
+    },
+    gener: {
+        type: String,
+        enum: ['male', 'female']
+    },
+    image: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        match: /.+@.+\..+/,
+        trim : true 
+    },
+    address: {
+        type: Schema.Types.ObjectId,
+        ref: 'address'
+    },
+    create_at: {
+        type: Date, default: Date.now
+    },
+    last_update: {
+        type: Date, default: Date.now
+    },
+    enrollment: {
+        type: Number
+    },
+    password: {
+        type: String
+    },
+    group: {
+        type: Schema.Types.ObjectId,
+        ref: 'group'
+    },
+    status: {
+        type: Boolean
+    },
+    permission: {
+        type: String,
+        enum: ['admin', 'teacher', 'parent']
+    }
 })
 
-const User = mongoose.model( "user", userSchema )
+const User = mongoose.model("users", userSchema)
